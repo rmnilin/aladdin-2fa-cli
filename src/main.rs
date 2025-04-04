cc4021f3bd90f9c2ea0e12b2ca681a8374911299
Roman Ilin <me@romanilin.me>
2025-04-04 14:25:44 +0300

    Add tracing and update daemon mode
    Add tracing, `verbose` flag, and make daemon mode keep running on errors
    related to HTTP requests.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use clap::{Args as ClapArgs, Parser, Subcommand};
+use std::str::FromStr;
+
+use clap::{ArgAction, Args as ClapArgs, Parser, Subcommand as ClapSubcommand};
 use color_eyre::Result;
+use tracing_subscriber::EnvFilter;
 
-mod commands;
 mod jas;
+mod subcommands;
 
-#[derive(Debug, Subcommand)]
-enum Command {
+#[derive(Debug, Parser)]
+#[command(name = env!("CARGO_BIN_NAME"), version, about, long_about = None)]
+struct Args {
+    /// CA certificate to verify server against
+    #[arg(short, long)]
+    ca_cert_path: Option<String>,
+
+    /// Retry count for failed requests
+    #[arg(short, long)]
+    retries: Option<u8>,
+
+    /// Logging level
+    #[arg(short, long, action = ArgAction::Count)]
+    verbose: u8,
+
+    #[command(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Debug, ClapSubcommand)]
+enum Subcommand {
     /// Register a new ticket by token URI and save ticket data to a JSON file
     #[command(arg_required_else_help = true)]
     Register {
         /// Path to save the ticket data
         #[arg(short, long)]
         file_path: String,
+
         /// Token URI to register
         uri: String,
     },
+
     /// Get current authentication requests
     #[command(arg_required_else_help = true)]
     Update {
@@ -22,70 +46,103 @@ enum Command {
         #[arg(short, long)]
         file_path: String,
     },
+
     /// Accept an authentication requests
     #[command(arg_required_else_help = true)]
     Accept {
         /// Path to ticket file
         #[arg(short, long)]
         file_path: String,
+
         #[command(flatten)]
-        args: AcceptArgs,
+        flatten_args: AcceptFlattenArgs,
     },
 }
 
 #[derive(Debug, ClapArgs)]
 #[group(required = true, multiple = false)]
-struct AcceptArgs {
+struct AcceptFlattenArgs {
     /// Session ID from the authentication request received with `update` command
     session_id: Option<String>,
+
     /// Run in daemon mode and automatically accept all requests
     #[arg(short, long)]
     daemon: bool,
 }
 
-#[derive(Debug, Parser)]
-#[command(name = env!("CARGO_BIN_NAME"))]
-#[command(about = "CLI to manage Aladdin 2FA tickets", long_about = None)]
-struct Args {
-    /// CA certificate to verify server against
-    #[arg(short, long)]
-    ca_cert_path: Option<String>,
-    /// Retry count for failed requests
-    #[arg(short, long)]
-    retries: Option<u8>,
-    #[command(subcommand)]
-    command: Command,
+fn init_tracing(verbose: u8) -> Result<()> {
+    use tracing::Level;
+    use tracing_subscriber::{fmt::layer, layer::SubscriberExt, registry, util::SubscriberInitExt};
+
+    let level = match verbose {
+        0 => Level::INFO.as_str(),
+        1 => Level::DEBUG.as_str(),
+        _ => Level::TRACE.as_str(),
+    };
+
+    let dependencies_level = match verbose {
+        0..=1 => "OFF",
+        2 => Level::ERROR.as_str(),
+        3 => Level::WARN.as_str(),
+        4 => Level::INFO.as_str(),
+        5 => Level::DEBUG.as_str(),
+        _ => Level::TRACE.as_str(),
+    };
+
+    let format_layer = layer().pretty();
+
+    #[cfg(not(feature = "err-loc"))]
+    let format_layer = format_layer
+        .compact()
+        .with_target(false)
+        .with_line_number(false)
+        .with_file(false);
+
+    registry()
+        .with(EnvFilter::from_str(
+            format!(
+                "{},{}::subcommands={}",
+                dependencies_level,
+                env!("CARGO_CRATE_NAME"),
+                level
+            )
+            .as_str(),
+        )?)
+        .with(format_layer)
+        .init();
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let args = Args::parse();
+    let Args {
+        subcommand,
+        ca_cert_path,
+        retries,
+        verbose,
+    } = Args::parse();
 
-    match args.command {
-        Command::Register { file_path, uri } => {
-            commands::register(&uri, &file_path, args.ca_cert_path.as_deref(), args.retries)
-                .await?;
+    init_tracing(verbose)?;
+
+    match subcommand {
+        Subcommand::Register { file_path, uri } => {
+            subcommands::register(&uri, &file_path, ca_cert_path.as_deref(), retries).await?;
         }
-        Command::Update { file_path } => {
-            commands::update(&file_path, args.ca_cert_path.as_deref(), args.retries).await?;
+        Subcommand::Update { file_path } => {
+            subcommands::update(&file_path, ca_cert_path.as_deref(), retries).await?;
         }
-        Command::Accept {
+        Subcommand::Accept {
             file_path,
-            args: AcceptArgs { daemon, session_id },
+            flatten_args: AcceptFlattenArgs { session_id, daemon },
         } => {
             if let Some(session_id) = session_id {
-                commands::accept(
-                    &file_path,
-                    &session_id,
-                    args.ca_cert_path.as_deref(),
-                    args.retries,
-                )
-                .await?;
-            } else if daemon {
-                commands::run_daemon(&file_path, args.ca_cert_path.as_deref(), args.retries)
+                subcommands::accept(&file_path, &session_id, ca_cert_path.as_deref(), retries)
                     .await?;
+            } else if daemon {
+                subcommands::run_daemon(&file_path, ca_cert_path.as_deref(), retries).await?;
             } else {
                 unreachable!("`session_id` and `daemon` arguments are mutually exclusive");
             }
---
+++ src/subcommands.rs
@@ -0,0 +1,91 @@
+use std::time::Duration;
+
+use color_eyre::Result;
+
+use crate::jas::{Client, Ticket};
+
+pub async fn register(
+    uri: &str,
+    file_path: &str,
+    ca_cert_path: Option<&str>,
+    retries: Option<u8>,
+) -> Result<()> {
+    let client = Client::new(ca_cert_path, retries)?;
+    let ticket = client.register(uri).await?;
+    ticket.save(file_path)?;
+    tracing::info!("Ticket registered and saved to {}", file_path);
+    Ok(())
+}
+
+pub async fn update(
+    file_path: &str,
+    ca_cert_path: Option<&str>,
+    retries: Option<u8>,
+) -> Result<()> {
+    let ticket = Ticket::load(file_path)?;
+    let client = Client::new(ca_cert_path, retries)?;
+    let session_ids = client.update(&ticket).await?;
+    if session_ids.is_empty() {
+        tracing::debug!("No authentication requests received");
+    } else {
+        for session_id in session_ids {
+            tracing::info!(
+                "Authentication request with session ID {} received",
+                session_id
+            );
+        }
+    }
+    Ok(())
+}
+
+pub async fn accept(
+    file_path: &str,
+    session_id: &str,
+    ca_cert_path: Option<&str>,
+    retries: Option<u8>,
+) -> Result<()> {
+    let ticket = Ticket::load(file_path)?;
+    let client = Client::new(ca_cert_path, retries)?;
+    client.accept(&ticket, session_id).await?;
+    tracing::info!(
+        "Authentication request with session ID {} accepted",
+        session_id
+    );
+    Ok(())
+}
+
+async fn run_daemon_iteration(ticket: &Ticket, client: &Client) -> Result<()> {
+    let session_ids = client.update(ticket).await?;
+    if session_ids.is_empty() {
+        tracing::debug!("No authentication requests received");
+    } else {
+        for session_id in client.update(ticket).await? {
+            tracing::debug!(
+                "Authentication request with session ID {} received",
+                session_id
+            );
+            client.accept(ticket, &session_id).await?;
+            tracing::info!(
+                "Authentication request with session ID {} accepted",
+                session_id
+            );
+        }
+    }
+    Ok(())
+}
+
+pub async fn run_daemon(
+    file_path: &str,
+    ca_cert_path: Option<&str>,
+    retries: Option<u8>,
+) -> Result<()> {
+    let ticket = Ticket::load(file_path)?;
+    let client = Client::new(ca_cert_path, retries)?;
+    loop {
+        let result = run_daemon_iteration(&ticket, &client).await;
+        if let Err(error) = result {
+            tracing::error!("{}", error);
+        };
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
